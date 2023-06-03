b998d8c654f062c800fba1af4dc0f0dd9997b8a8
Andres Trujillo <andretru2@gmail.com>
2023-06-03 16:47:33 -0400

    app folder

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+// import { ClerkProvider } from "@clerk/nextjs/app-beta";
+
+export const metadata = {
+  title: "Create T3 Modern",
+  description: "Theo got bored again",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* <ClerkProvider> */}
+      <body>{children}</body>
+      {/* </ClerkProvider> */}
+    </html>
+  );
+}
