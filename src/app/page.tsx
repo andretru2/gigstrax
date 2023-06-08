503a0472ffa7ceb107876d15761fccc2bd2d2f2a
Andres Trujillo <andretru2@gmail.com>
2023-06-07 20:56:54 -0400

    build success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { prisma } from "@/server/db";
 
 export default async function prismaExample() {
-  const res = await prisma.user.create({
-    data: {
-      name: "Elliott",
-      email: "at@example-user.com",
-    },
-  });
-  console.log(res);
+  // const res = await prisma.user.create({
+  //   data: {
+  //     name: "Elliott",
+  //     email: "at@example-user.com",
+  //   },
+  // });
+  // console.log(res);
 
   const users = await prisma.user.findMany();
 
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -94,17 +94,17 @@ export const createTRPCRouter = t.router;
 export const publicProcedure = t.procedure;
 
 /** Reusable middleware that enforces users are logged in before running the procedure. */
-const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
-    },
-  });
-});
+// const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
+//   if (!ctx.session || !ctx.session.user) {
+//     throw new TRPCError({ code: "UNAUTHORIZED" });
+//   }
+//   return next({
+//     ctx: {
+//       // infers the `session` as non-nullable
+//       session: { ...ctx.session, user: ctx.session.user },
+//     },
+//   });
+// });
 
 /**
  * Protected (authenticated) procedure
@@ -114,4 +114,4 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
  *
  * @see https://trpc.io/docs/procedures
  */
-export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
+// export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
