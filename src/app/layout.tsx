b9e52fcb837ace292099c5cb89c1727596bf817c
Andres Trujillo <andretru2@gmail.com>
2023-06-09 09:22:34 -0400

    one more testx2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 // import { ClerkProvider } from "@clerk/nextjs/app-beta";
 
 export const metadata = {
-  title: "Create T3 Modernssss",
+  title: "Create T3 Modernsssss",
   description: "Theo got bored again",
 };
 
