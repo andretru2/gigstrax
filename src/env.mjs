a7c1cc2d7ff9b400bc59ee63f718bf5b96a5d626
Andres Trujillo <andretru2@gmail.com>
2023-09-13 11:16:30 -0400

    add middleware

--- src/env.mjs
+++ src/env.mjs
@@ -41,6 +41,7 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_APP_ENV: z.string(),
 
     // NEXT_PUBLIC_AUTH_URL: z.string().url(),
   },
@@ -51,6 +52,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     POSTGRES_URL: process.env.POSTGRES_URL,
