4baa4b15d602a15290debbfcad25f96692238c9c
Andres Trujillo <andretru2@gmail.com>
2023-09-30 18:51:36 -0400

    test timefix again

--- src/app/(dashboard)/dashboard/gigs/[gigId]/page.tsx
+++ src/app/(dashboard)/dashboard/gigs/[gigId]/page.tsx
@@ -72,7 +72,7 @@ export default async function Page({ params }: Props) {
   const formattedDate =
     gig?.gigDate && formatDate(gig?.gigDate.getTime(), "friendly");
 
-  const startTime = gig.timeStart && formatTime(gig.timeStart);
+  const startTime = gig.timeStart && formatTime(fromUTC(gig.timeStart));
   const endTime = gig?.timeEnd && formatTime(gig?.timeEnd);
 
   const clientName = client?.client ?? "";
